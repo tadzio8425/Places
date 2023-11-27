1fe217036d930a16848b0079e2fa98cfd53d8add
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-11-26 19:48:25 -0500

    FIX

--- ISIS2503-Microservices-AppDjango-master/places/place/urls.py
+++ ISIS2503-Microservices-AppDjango-master/places/place/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from django.conf.urls import include
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import re_path as url
-
 from . import views
 
 urlpatterns = [
-    url(r'^variables/', views.VariableList, name='variableList'),
-    url(r'^variablecreate/$', csrf_exempt(views.VariableCreate), name='variableCreate'),
+    url(r'^places/', views.PlaceList, name='placesList'),
+    url(r'^placecreate/$', csrf_exempt(views.PlaceCreate), name='placeCreate'),
 ]
--- ISIS2503-Microservices-AppDjango-master/places/places/urls.py
+++ ISIS2503-Microservices-AppDjango-master/places/places/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('places/', views.PlaceList, name='placesList'),
-    path('placecreate/', csrf_exempt(views.PlaceCreate), name='placeCreate'),
+    path('', include('place.urls')),
 ]
 
