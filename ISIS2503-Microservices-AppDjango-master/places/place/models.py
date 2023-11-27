487e7314d0a21438139eb222ab16cec20ca008c1
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-11-26 20:17:40 -0500

    id fix

--- ISIS2503-Microservices-AppDjango-master/places/place/models.py
+++ ISIS2503-Microservices-AppDjango-master/places/place/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Place(models.Model):
-    id = models.AutoField(primary_key=True)
+
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return '{}'.format(self.name)
